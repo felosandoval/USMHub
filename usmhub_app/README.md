3f2a1f0fe680b2ed91a555cf9126a664482c3451
felosandoval <sandovalcornejo9@gmail.com>
2024-10-07 18:32:49 -0300

    Inicio

---
+++ usmhub_app/README.md
@@ -0,0 +1,3 @@
+# usmhub_app
+
+A new Flutter project.
---
+++ usmhub_app/android/app/src/main/kotlin/com/example/usmhub_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.usmhub_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
